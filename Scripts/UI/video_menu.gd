15c4a4a2e6a00e226af4563c818c5a42d40906a6
Lara Narvaez Otero <l.narvaez.otero@gmail.com>
2026-01-31 03:27:54 +0100

    pantalla fullscreen

--- Scripts/UI/video_menu.gd
+++ Scripts/UI/video_menu.gd
@@ -1,7 +1,16 @@
 extends Control
 
 
+@onready var option_button: CheckButton = $HBoxContainer/Container2/OptionButton
 
 
 func _on_back_button_pressed() -> void:
 	visible = false
+
+
+
+func _on_option_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
