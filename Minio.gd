37e77cdce3219f6a0558b2b9e0a7d3d78e7dc383
smorcor744 <mcsergio@iesrafaelalberti.es>
2026-01-31 02:20:36 +0100

    move

--- Minio.gd
+++ Minio.gd
@@ -1,21 +1,51 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var tipo : String = ""
+@export var speed = 300
+
+@export var posicion : Vector2
 @onready var animacion: AnimatedSprite2D = $AnimatedSprite2D
 var list_tipos = {
 	"Fuego": "Agua", 
 	"Agua": "Tierra", 
-	"PLanta": "Fuego", 
+	"Planta": "Fuego", 
 	"Tierra": "Planta"}
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
 	tipo_random()
+	moverse()
+func _physics_process(_delta: float) -> void:
+	if position.distance_to(posicion) < 5:
+		velocity.x = 0
+	else:
+		moverse()
+	move_and_slide()
+	
+func handel_animacion():
+	if velocity.x != 0:
+		animacion.play(str(tipo,"_walk"))
+	else:
+		animacion.play(str(tipo,"_idle"))
+	if velocity.x > 0:
+		animacion.flip_h = false
+	if velocity.x < 0:
+		animacion.flip_h = true
+
+func moverse():
+	var direction = (posicion - global_position).normalized()
+	velocity.x = direction.x * speed
 
 func tipo_random():
 	if tipo == "":
 		randomize()
 		var lista = list_tipos.keys()
-		tipo = lista[randi_range(0,lista.size())]
+		tipo = lista[randi_range(0,lista.size()-1)]
+	elegir_animacion()
+
+func elegir_animacion():
+	animacion.play(str(tipo,"_idle"))
 
 func take_damage(atq_tipo):
 	if tipo == list_tipos[atq_tipo]:
@@ -23,4 +53,7 @@ func take_damage(atq_tipo):
 
 
 func muerte():
+	animacion.play(str(tipo,"_dead"))
+	await get_tree().create_timer(2).timeout
+	
 	queue_free()
