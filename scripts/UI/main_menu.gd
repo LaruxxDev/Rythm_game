cb549068e543bcb7fad265bf1b80d21d640ef6ff
Lara Narvaez Otero <l.narvaez.otero@gmail.com>
2026-02-01 12:01:19 +0100

    setting menu

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -6,8 +6,7 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(gameScene)
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/setting_menu.tscn")
-
+	$SettingMenu.visible = true
 
 func _on_leaderboard_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/leaderboard.tscn")
--- scripts/UI/setting_menu.gd
+++ scripts/UI/setting_menu.gd
@@ -16,12 +16,9 @@ func _on_sound_button_pressed() -> void:
 func _on_video_button_pressed() -> void:
 	video_menu.visible = true
 
-func _on_credits_button_pressed() -> void:
-	pass
-
 func _on_resume_button_pressed() -> void:
-	self.visible = false
+	visible = false
 
 
 func _on_exit_button_pressed() -> void:
-	self.visible = false
+	visible = false
